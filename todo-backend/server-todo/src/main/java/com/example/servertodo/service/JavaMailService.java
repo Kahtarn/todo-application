40e9f7eae6b0e2f4dc4e5525ff45ba2bdc91195e
kahtarn <0306231203@caothang.edu.vn>
2026-03-04 22:45:56 +0700

    backend: fixed setText on sendOtpRegister method

--- todo-backend/server-todo/src/main/java/com/example/servertodo/service/JavaMailService.java
+++ todo-backend/server-todo/src/main/java/com/example/servertodo/service/JavaMailService.java
@@ -12,9 +12,20 @@ public class JavaMailService {
 
     public void sendOtpRegister(String toEmail, String otp) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("your_email@gmail.com");
         message.setTo(toEmail);
-        message.setSubject("Verify your account");
-        message.setText("Your OTP code is: " + otp);
+        message.setSubject("[ServerTodo] Email Verification Code");
+
+        message.setText(
+                "Hello,\n\n" +
+                        "Thank you for registering at ServerTodo.\n\n" +
+                        "Your verification code is: " + otp + "\n" +
+                        "This code will expire in 5 minutes.\n\n" +
+                        "If you did not request this, please ignore this email.\n\n" +
+                        "Best regards,\n" +
+                        "ServerTodo Team"
+        );
+
         javaMailSender.send(message);
     }
 }
