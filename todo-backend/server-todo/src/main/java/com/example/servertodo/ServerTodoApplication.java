14a394c3c702d1804b720b20df61da27b88059c2
kahtarn <0306231203@caothang.edu.vn>
2026-03-04 15:23:16 +0700

    add folder frontend and backend to project

---
+++ todo-backend/server-todo/src/main/java/com/example/servertodo/ServerTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.servertodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerTodoApplication.class, args);
+    }
+
+}
---
+++ todo-backend/server-todo/src/test/java/com/example/servertodo/ServerTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servertodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
